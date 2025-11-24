d2ad38afd6a270f255c3bfbe2261bc0e647decea
Franck <franck.spadotto@yahoo.com>
2025-11-24 10:48:02 +0100

    feat(sum) list of float and int

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""
+6-sum_mixed_list.py
+"""
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    sum_mixed_list function
+    """
+    return (sum(mxd_lst))
