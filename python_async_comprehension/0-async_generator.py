2d3d885fe1b4b7494fd475f8226f6d24ab350c58
Franck <franck.spadotto@yahoo.com>
2025-11-24 15:18:52 +0100

    fix(return) try with Generator despite the error

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,14 +3,14 @@
 0-async_generator.py
 """
 import asyncio
-import random
-from typing import AsyncGenerator
+from random import uniform
+from typing import Generator, List
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     async generator function
     """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield uniform(0, 10)
