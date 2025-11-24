4835233c7d3445cb74b594ced88a9f3c44301e6b
Franck <franck.spadotto@yahoo.com>
2025-11-24 11:04:45 +0100

    feat(kv) complex type

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+7-to_kv.py
+"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    to_kv function
+    """
+    return (k, float(v ** 2))
