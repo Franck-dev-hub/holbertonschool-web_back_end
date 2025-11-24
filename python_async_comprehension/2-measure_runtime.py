6c37384e53fc45b9eb23c22d692e7cf1f26ff7d7
Franck <franck.spadotto@yahoo.com>
2025-11-24 15:05:20 +0100

    feat(runtime) measure runtime

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""
+2-measure_runtime.py
+"""
+import asyncio
+import time
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime():
+    """
+    measure runtime function
+    """
+    start = time.time()
+    await asyncio.gather(*[async_comprehension() for i in range(4)])
+    end = time.time()
+    return (end - start)
