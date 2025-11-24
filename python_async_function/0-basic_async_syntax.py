466ed9be4fe1601dc9261072f435674a0a465d55
Franck <franck.spadotto@yahoo.com>
2025-11-24 13:34:54 +0100

    feat(async) basic of async

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python
+"""
+0-basic_async_syntax.py
+"""
+import asyncio
+from random import uniform as rdm
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    wait random function
+    """
+    delay = rdm(0, max_delay)
+    await asyncio.sleep(delay)
+    return (delay)
