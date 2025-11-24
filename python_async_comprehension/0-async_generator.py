03b9e9b3857fb7201ddd5a10d33356f9b01b7e2a
Franck <franck.spadotto@yahoo.com>
2025-11-24 14:41:50 +0100

    feat(async) generate coroutines

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""
+0-async_generator.py
+"""
+import asyncio
+from random import uniform
+
+
+async def async_generator():
+    """
+    async generator function
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield uniform(0, 10)
