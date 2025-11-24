e81e7a1bd9dfeec25896f276fd2fe56472787439
Franck <franck.spadotto@yahoo.com>
2025-11-24 11:23:49 +0100

    feat(mult) return a func that multiplies 2 numbers

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""
+8-make_multiplier.py
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    make multiplier function
+    """
+    def function(n: float) -> float:
+        return (n * multiplier)
+
+    return (function)
