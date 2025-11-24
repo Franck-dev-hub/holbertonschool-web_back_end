bdf3aad11c0cce49a0bdc3298e5f3c51ea285221
Franck <franck.spadotto@yahoo.com>
2025-11-24 14:17:33 +0100

    feat(copy) copy 1-concurrent_coroutines.py into 4-task.py

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python
+"""
+4-tasks.py
+"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    task wait n function
+    """
+    tasks = [task_wait_random(max_delay) for i in range(n)]
+    results = await asyncio.gather(*tasks)
+    return sorted(results)
