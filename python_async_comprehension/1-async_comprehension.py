3002b3c8a46272420d6ab008ac7cbfeb92864816
Franck <franck.spadotto@yahoo.com>
2025-11-24 14:53:11 +0100

    feat(async) async comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+1-async_comprehension.py
+"""
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension():
+    """
+    async comprehension function
+    """
+    return ([x async for x in async_generator()])
