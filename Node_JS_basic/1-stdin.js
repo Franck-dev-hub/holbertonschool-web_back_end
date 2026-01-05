593c4636839202c7e239747e7f780a17ed0562dc
Franck S. <franck.spadotto@yahoo.com>
2026-01-05 11:18:53 +0100

    fix: use process

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,4 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-
-process.stdin.once("data", (name) => {
-  process.stdout.write(`Your name is: ${name}`);
-
-  if (!process.stdin.isTTY) {
-    process.stdout.write("This important software is now closing\n");
-  }
-
-  process.exit(0);
+process.stdout.write("Welcome to Holberton School, what is your name?\n", () => {
+  process.stdin.on("data", (name) => process.stdout.write(`Your name is: ${name}`));
+  process.stdin.on("end", () => process.stdout.write('This important software is now closing\n'));
 });
