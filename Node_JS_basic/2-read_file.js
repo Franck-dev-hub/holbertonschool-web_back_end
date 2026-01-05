b29a6722b091ea512b46b242ab58a27a1530f09a
Franck S. <franck.spadotto@yahoo.com>
2026-01-05 14:02:05 +0100

    fix: filter lines lately & manual count total

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,29 +2,33 @@ const fs = require("fs");
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, "utf-8")
-    const lines = data.split('\n').filter(line => line.trim());
+    const data = fs.readFileSync(path, "utf-8");
+    const lines = data.split('\n');
 
     // Remove header to count students
-    const students = lines.slice(1);
-    console.log(`Number of students: ${students.length}`);
-
-    // Group field
     const fields = {};
-    students.forEach(line => {
+    let total = 0;
+
+    for (let i = 1; i < lines.length; i += 1) {
+      const line = lines[i].trim();
+      if (line.length === 0) continue;
+
       const [firstname, lastname, age, field] = line.split(',');
       if (!fields[field]) {
         fields[field] = [];
       }
       fields[field].push(firstname);
-    });
+      total += 1;
+    }
 
-  // Log by field
+    console.log(`Number of students: ${total}`);
+
+    // Log by field
     Object.entries(fields).forEach(([field, names]) => {
-      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(", ")}`);
     });
-  } catch {
-    throw new Error('Cannot load the database');
+  } catch (err) {
+    throw new Error("Cannot load the database");
   }
 }
 
