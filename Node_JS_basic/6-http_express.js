001d6efdfc7baa56ee77bd4169388dda305a6297
Franck S. <franck.spadotto@yahoo.com>
2026-01-05 16:48:00 +0100

    feat: use express to make a http request

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 1245;
+
+// Basic route to test the server
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+module.export = app;
