3e679d933db96dbfa949b48cddc4ae4920d4f4e0
Franck S. <franck.spadotto@yahoo.com>
2026-01-05 17:05:29 +0100

    fix: synthax errors

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const countStudent = require('./3-read_file_async');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-  res.set('Content-type', 'test/plain');
+  res.set('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
   const file = process.argv[2];
 
-  res.set('Content-type', 'test/plain');
+  res.set('Content-Type', 'text/plain');
   countStudents(file)
     .then((data) => res.send(`This is the list of our students\n${data}`))
-    .then((err) => res.send(`This is the list of our students\n${err.message}`));
+    .catch((err) => res.send(`This is the list of our students\n${err.message}`));
 });
 
 app.listen(port, () => {
