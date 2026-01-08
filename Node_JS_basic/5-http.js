faecd9f72918fecfb7190e6bc72579f4d9ef8013
Franck S. <franck.spadotto@yahoo.com>
2026-01-08 09:57:27 +0100

    fix: change request and response into req and res

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,17 +1,17 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((request, response) => {
+const app = http.createServer((req, res) => {
   const file = process.argv[2];
-  const { url } = request;
+  const { url } = req;
 
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (url === '/') {
-    response.end('Hello Holberton School!');
+    res.end('Hello Holberton School!');
   } else if (url === '/students') {
     countStudents(file)
-      .then((data) => response.end(`This is the list of our students\n${data}`))
-      .catch((err) => response.end(`This is the list of our students\n${err.message}`));
+      .then((data) => res.end(`This is the list of our students\n${data}`))
+      .catch((err) => res.end(`This is the list of our students\n${err.message}`));
   }
 });
 
