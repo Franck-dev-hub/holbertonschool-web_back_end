288be7a9e4e247416e84cfe8a2087cd46d172bd4
Franck S. <franck.spadotto@yahoo.com>
2026-01-08 09:50:44 +0100

    fix: remove port variable

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,8 +1,6 @@
 const http = require('node:http');
 const countStudents = require('./3-read_file_async');
 
-const port = 1245;
-
 const app = http.createServer((request, response) => {
   const file = process.argv[2];
   const { url } = request;
@@ -17,8 +15,8 @@ const app = http.createServer((request, response) => {
   }
 });
 
-app.listen(port, 'localhost', () => {
-  console.log(`Server running at http://localhost:${port}/`);
+app.listen(1245, 'localhost', () => {
+  console.log(`Server running at http://localhost:1245/`);
 });
 
 module.exports = app;
